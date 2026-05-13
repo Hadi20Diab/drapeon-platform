640350b0799160630a3f3c58c67f810a9e02d4fb
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2026-05-13 16:18:09 +0300

    vercel web adapter

---
+++ apps/web/src/entry.vercel-edge.tsx
@@ -0,0 +1,13 @@
+import { createQwikCity } from "@builder.io/qwik-city/middleware/vercel-edge";
+import type { PlatformVercel } from "@builder.io/qwik-city/middleware/vercel-edge";
+import qwikCityPlan from "@qwik-city-plan";
+import render from "./entry.ssr";
+
+declare global {
+  type QwikCityPlatform = PlatformVercel;
+}
+
+export default createQwikCity({
+  render,
+  qwikCityPlan,
+});
